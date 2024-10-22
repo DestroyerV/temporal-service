26b803ea5dd6fab1db4e1d4b994b32fface9ab82
Vaibhav Mishra <95367218+DestroyerV@users.noreply.github.com>
2024-10-22 13:04:02 +0000

    Refactor API endpoints to use arrow function syntax

--- index.js
+++ index.js
@@ -3,33 +3,28 @@ import path from "path";
 import cors from "cors";
 
 const __dirname = path.resolve();
-
 const app = express();
 
 app.use(cors());
-
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-// first API endpoint...
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello", (req, res) => {
   res.json({ greeting: "hello API" });
 });
 
-// current date endpoint
-app.get("/api", function (req, res) {
-  let currentDate = new Date();
+app.get("/api", (req, res) => {
+  const currentDate = new Date();
   res.json({
-    unix: parseInt(currentDate.getTime()),
+    unix: currentDate.getTime(),
     utc: currentDate.toUTCString(),
   });
 });
 
-// Date endpoint
-app.get("/api/:date", function (req, res) {
+app.get("/api/:date", (req, res) => {
   let dateString = req.params.date;
 
   // Check if the input is a number (timestamp)
@@ -39,15 +34,16 @@ app.get("/api/:date", function (req, res) {
 
   let date = new Date(dateString);
 
-  // Check if the date is valid
   if (!isNaN(date.getTime())) {
-    res.json({ unix: parseInt(date.getTime()), utc: date.toUTCString() });
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString(),
+    });
   } else {
     res.json({ error: "Invalid Date" });
   }
 });
 
-// Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
